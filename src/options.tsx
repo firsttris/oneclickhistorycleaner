329e4125dfb861e628c99698b120f6787af1b1c6
Tristan Teufel <tristanteufel@googlemail.com>
2023-12-11 12:26:10 +0100

    init cleanup

--- src/options.tsx
+++ src/options.tsx
@@ -3,8 +3,4 @@ import { Settings } from './components/Settings';
 import '@fontsource/roboto';
 
 const element = document.getElementById('root');
-if (element) {
-    render(() => <Settings />, element);
-} else {
-    console.error(`Element with id root not found`);
-}
+render(() => <Settings />, element!);
